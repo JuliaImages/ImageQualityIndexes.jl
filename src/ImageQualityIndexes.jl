dd469aea94337ac50e1f00bc12eea1f5294a84ee
Johnny Chen <johnnychen94@hotmail.com>
2019-05-19 16:50:46 +0800

    Files generated by PkgTemplates

---
+++ src/ImageQualityIndexes.jl
@@ -0,0 +1,5 @@
+module ImageQualityIndexes
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ImageQualityIndexes
+using Test
+
+@testset "ImageQualityIndexes.jl" begin
+    # Write your own tests here.
+end
