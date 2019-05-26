d8f0d8a4e99e07be9a5d1c0d196909f00617632f
Johnny Chen <johnnychen94@hotmail.com>
2019-05-27 01:24:28 +0800

    add ssim

--- src/ImageQualityIndexes.jl
+++ src/ImageQualityIndexes.jl
@@ -1,12 +1,15 @@
 module ImageQualityIndexes
 
-using MappedArrays
-using ImageCore
+using MappedArrays, OffsetArrays
+using ImageCore, ColorVectorSpace
 using ImageCore: NumberLike, Pixel, GenericImage, GenericGrayImage
-using ImageDistances
+using ImageDistances, ImageFiltering
+using Statistics: mean
+using Base.Iterators: repeated
 
 include("generic.jl")
 include("psnr.jl")
+include("ssim.jl")
 
 export
     # generic
@@ -14,6 +17,10 @@ export
 
     # Peak signal-to-noise ratio
     PSNR,
-    psnr
+    psnr,
+
+    # Structral Similarity
+    SSIM,
+    ssim
 
 end # module
