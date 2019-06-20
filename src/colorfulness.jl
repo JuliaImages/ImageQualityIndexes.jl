17ee2bbb30913b9b21cf13e17ed8e8fd0872b9c6
Rob Blackwell <rob@robblackwell.com>
2019-06-20 11:44:10 +0100

    Updated docstrings

--- src/colorfulness.jl
+++ src/colorfulness.jl
@@ -2,19 +2,19 @@
 
     HASLER_AND_SUSSTRUNK_M3 <: NoReferenceIQI
     M =  hasler_and_susstrunk_m3(img)
-    M =  colorfulness(img)
 
-Calculates the colorfulness of a natural RGB image according to the
-metric, M3 from [1]. As a guide to interpretation of results, the
-authors suggest:
+Calculates the colorfulness of a natural image using method M3 from
+[1]. As a guide to interpretation of results, the authors suggest:
 
-|Not colorful        |  0|
-|slightly colorful   | 15|
-|moderately colorful | 33|
-|averagely colorful  | 45|
-|quite colorful      | 59|
-|highly colorful     | 82|
-|extremely colorful  |109|
+| Attribute           | M3 |
+|:--------------------|----|
+| Not colorful        |  0 |
+| slightly colorful   | 15 |
+| moderately colorful | 33 |
+| averagely colorful  | 45 |
+| quite colorful      | 59 |
+| highly colorful     | 82 |
+| extremely colorful  |109 |
 
 [1] Hasler, D. and Süsstrunk, S.E., 2003, June. Measuring colorfulness
 in natural images. In Human vision and electronic imaging VIII
@@ -51,28 +51,17 @@ end
 hasler_and_susstrunk_m3(img::GenericGrayImage) = 0
 
 """
+     M =  colorfulness(img)
      M =  colorfulness(HASLER_AND_SUSSTRUNK_M3(), img)
 
-Measures the colorfulness of a natural image using Method 3 from [1].
+Measures the colorfulness of a natural image. Uses the
+[`HASLER_AND_SUSSTRUNK_M3`](@ref) method by default.
 
-[1] Hasler, D. and Süsstrunk, S.E., 2003, June. Measuring colorfulness
-in natural images. In Human vision and electronic imaging VIII
-(Vol. 5007, pp. 87-96). International Society for Optics and
-Photonics.
+See also: [`HASLER_AND_SUSSTRUNK_M3`](@ref).
+
 """
 colorfulness(m::HASLER_AND_SUSSTRUNK_M3, img) = m(img)
 
-"""
-     M =  colorfulness(img)
-
-Measures the colorfulness of a natural image using the default
-algorithm which is currently Method 3 from [1].
-
-[1] Hasler, D. and Süsstrunk, S.E., 2003, June. Measuring colorfulness
-in natural images. In Human vision and electronic imaging VIII
-(Vol. 5007, pp. 87-96). International Society for Optics and
-Photonics.
-"""
 colorfulness(img) = HASLER_AND_SUSSTRUNK_M3()(img)
 
 
