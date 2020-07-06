07697d64a36e65e22ab2c792834f1c9029783f65
jiviteshjain <jiviteshjain@outlook.com>
2020-07-06 13:33:11 +0530

    msssim: fix bug, add tests

--- test/msssim.jl
+++ test/msssim.jl
@@ -17,16 +17,16 @@ using ImageFiltering
     @test (iqi_a.kernel == iqi_b.kernel == iqi_c.kernel == iqi_d.kernel) &&
           (iqi_a.W == iqi_b.W == iqi_c.W == iqi_d.W)
 
-    # Gray images
+    # Gray test images
     img1 = testimage("cameraman")
     img2 = testimage("lena_gray_512")
-    @test assess_msssim(img1, img2) ≈ 0.0517 atol=1e-4
+    @test assess_msssim(img1, img2) ≈ 0.0711 atol=1e-4
     @test assess(MSSSIM(), img1, img2) ≈ MSSSIM()(img1, img2)
 
-    # RGB images
+    # RGB test images
     img3 = testimage("mandril_color")
     img4 = testimage("lena_color_512")
-    @test assess_msssim(img3, img4) ≈ 0.0192 atol=1e-4
+    @test assess_msssim(img3, img4) ≈ 0.0219 atol=1e-4
     @test assess(MSSSIM(), img3, img4) ≈ MSSSIM()(img3, img4)
 
     # Varying αᵢ, βᵢ, γᵢ
@@ -35,10 +35,10 @@ using ImageFiltering
         (0.3001, 0.0448, 0.2856),
         (0.2856, 0.3001, 0.0448),
     ))
-    @test iqi_e(img3, img4) ≈ 0.0852 atol=1e-4
+    @test iqi_e(img3, img4) ≈ 0.0878 atol=1e-4
     # non standard parameters, result may differ from other implementations
 
-    # Gray Image
+    # Gray type tests
     type_list = generate_test_types([Float32, N0f8], [Gray])
     A = rand(128,128)
     B = rand(128,128)
@@ -52,4 +52,22 @@ using ImageFiltering
         @test assess_msssim(a, a) ≈ 1.0
     end
 
+
+    # RGB type tests
+    type_list = generate_test_types([Float32, N0f8], [RGB])
+    A = [RGB(0.0, 0.0, 0.0) RGB(0.0, 1.0, 0.0) RGB(0.0, 1.0, 1.0)
+        RGB(0.0, 0.0, 1.0) RGB(1.0, 0.0, 0.0) RGB(1.0, 1.0, 0.0)
+        RGB(1.0, 1.0, 1.0) RGB(1.0, 0.0, 1.0) RGB(0.0, 0.0, 0.0)]
+    B = [RGB(0.0, 0.0, 0.0) RGB(0.0, 0.0, 1.0) RGB(1.0, 1.0, 1.0)
+        RGB(0.0, 1.0, 0.0) RGB(1.0, 0.0, 0.0) RGB(1.0, 0.0, 1.0)
+        RGB(0.0, 1.0, 1.0) RGB(1.0, 1.0, 0.0) RGB(0.0, 0.0, 0.0)]
+
+    for T in type_list
+
+        a = A .|> T
+        b = B .|> T
+
+        @test assess_msssim(a, b) == assess(MSSSIM(), a, b) == MSSSIM()(a, b)
+        @test assess_msssim(a, a) ≈ 1.0
+    end
 end
