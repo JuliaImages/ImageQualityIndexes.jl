b77cc921d0d152a31f663837dc5e20cddc3d00e2
Ashwani Rathee <ab669522@gmail.com>
2021-10-04 20:46:45 +0530

    fsim similarity measure

--- src/fsim.jl
+++ src/fsim.jl
@@ -1,6 +1,16 @@
 # using ImagePhaseCongruency
 # using Images
 
+function fsim_similarity_measure(x, y, constant)
+    """
+    Calculate feature similarity measurement between two images
+    """
+    numerator = 2 * x * y .+ constant
+    denominator = x^2 .+ y^2 .+ constant 
+
+	return numerator ./ denominator
+end
+
 function fsim(org_img, pred_img, T1=0.85, T2=160)
     # alpha,beta parameters used to adjust the relative importance of PC and GM features
     alpha = 1 
@@ -13,8 +23,8 @@ function fsim(org_img, pred_img, T1=0.85, T2=160)
        pcp_sum = M2
        grad1x, grad1y, gm1, orient1 = imedge(i.(org_img))
        grad2x, grad2y, gm2, orient2 = imedge(i.(pred_img))
-       S_pc = _similarity_measure(pco_sum, pcp_sum, T1)
-       S_g = _similarity_measure(gm1, gm2, T2)
+       S_pc = fsim_similarity_measure(pco_sum, pcp_sum, T1)
+       S_g = fsim_similarity_measure(gm1, gm2, T2)
        S_l = (S_pc^alpha) .* (S_g^beta)
        numerator = sum(S_l .* map(max,pco_sum, pcp_sum))
        denominator = sum(map(max,pco_sum, pcp_sum))
