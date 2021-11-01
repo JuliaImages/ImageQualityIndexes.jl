96135cd2c3edde549939e39055b4c75e63eeb525
Johnny Chen <johnnychen94@hotmail.com>
2021-11-01 14:16:48 +0800

    remove depwarn for `psnr` and `ssim` (#43)

--- src/deprecations.jl
+++
@@ -1,5 +0,0 @@
-# renaming `psnr` and `ssim` following the general guideline
-# > for functions that compute something or perform an operation, start with a verb
-# https://github.com/JuliaImages/Images.jl/issues/767
-Base.@deprecate_binding psnr assess_psnr
-Base.@deprecate_binding ssim assess_ssim
--- test/deprecations.jl
+++
@@ -1,7 +0,0 @@
-@testset "deprecations" begin
-    @info "depwarns are expected"
-    img1 = testimage("cameraman")
-    img2 = testimage("lena_gray_512")
-    @test ssim(img1, img2) == assess_ssim(img1, img2)
-    @test psnr(img1, img2) == assess_psnr(img1, img2)
-end
